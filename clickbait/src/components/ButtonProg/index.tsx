64bbf5446e8980274f41e022786c3103340ed146
299792455 <75017pi@gmail.com>
2025-07-17 21:40:41 +0200

    database + lib constants.ts

---
+++ clickbait/src/components/ButtonProg/index.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+export default function ButtonProg() {
+
+
+
+    return (
+        <div>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">ButtonProg</button>
+        </div>
+    )
+}
